da056f123349b57083844df7ec7889bc1154c778
Shriram Krishnamurthi <sk@cs.brown.edu>
2010-10-08 08:53:24 -0400

    scratch area, ignore

--- scratch-area.rkt
+++ scratch-area.rkt
@@ -1,7 +1,29 @@
 #lang s-exp "p4p.rkt"
 
-deffun: tryand(x) =
-  and: { false, 
-         /(1, x) }
+sign: is-a-nelon? is: {Any -> Boolean}
+deffun: is-a-nelon?(l) =
+  not(empty?(l))
+  
+defsigpred: NELON = is-a-nelon?
 
-tryand(0)
+sign: some-nelon is: {NELON}
+defvar: some-nelon = list(1, 2, 3)
+
+defsigpred: NELON2 = fun: (l) in: not(empty?(l))
+
+sign: first-num is: {NELON2 -> Number}
+deffun: first-num(a-nelon)
+  first(a-nelon)
+
+#|
+Things that won't work:
+
+define: NELON = signature: predicate: is-a-nelon?
+
+defvar: 1stNumSig = signature: {predicate: is-a-nelon? -> Number}
+
+sign: 1st-num is: 1stNumSig
+deffun: 1st-num(a-nelon)
+  first(a-nelon)
+  
+|#
